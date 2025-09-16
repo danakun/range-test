2954d64ae247f5e56bdd0c0dd9742d87201ca9a3
Dana Kun <dana.liepinsh@gmail.com>
2025-09-17 01:51:12 +0200

    change the port

--- src/lib/endpoints.ts
+++ src/lib/endpoints.ts
@@ -1,16 +1,6 @@
-const IS_SERVER = typeof window === 'undefined';
-let baseURL = '';
-
-if (IS_SERVER) {
-  baseURL = process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8080';
-} else {
-  // On the client use the relative path
-  baseURL = process.env.NEXT_PUBLIC_API_BASE_URL || '';
-}
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export const API_ENDPOINTS = {
-  fixedValues: `${baseURL}/api/fixed-values`,
-  rangeConfig: `${baseURL}/api/range-config`
+  fixedValues: `${API_BASE_URL}/api/fixed-values`,
+  rangeConfig: `${API_BASE_URL}/api/range-config`
 };
